edd87e82a185e1d4e6b56c6cd6239384aa77ddc3
stefan_savic <stevjah11@gmail.com>
2017-04-14 19:40:20 +0200

    'intall-uglify-css'

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 var sass        = require('gulp-sass');
 var wait = require('gulp-wait');
+var uglifycss = require('gulp-uglifycss');
 var bs = require('browser-sync').create();
- 
- 
+
+
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
     //watch files
@@ -14,29 +15,33 @@ gulp.task('browser-sync', function() {
     './*.html',
     './*.js'
     ];
- 
+
     bs.init({
         server: {
             baseDir: "./"
         }
+        });
     });
-});
- 
+
 // Sass task, will run when any SCSS files change & BrowserSync
 // will auto-update browsers
 gulp.task('sass', function () {
-    return gulp.src(['sass/*.scss','sass/*.sass'])  
-        .pipe(wait(500))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./'))
-        .pipe(bs.reload({stream:true}));
-});
- 
+    return gulp.src(['sass/main.scss'])  
+    .pipe(wait(500))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": false
+      }))
+    .pipe(gulp.dest('./'))
+    .pipe(bs.reload({stream:true}));
+    });
+
 gulp.task('javascript',function(){
-        return gulp.src('js/*.js')
-        .pipe(gulp.dest('./'))
-        .pipe(bs.reload({stream:true}));
-});
+    return gulp.src('js/*.js')
+    .pipe(gulp.dest('./'))
+    .pipe(bs.reload({stream:true}));
+    });
 
 // Default task to be run with `gulp`
 gulp.task('default', ['sass','browser-sync','javascript'], function () {
@@ -44,5 +49,5 @@ gulp.task('default', ['sass','browser-sync','javascript'], function () {
     gulp.watch('js/**/*.js', ['javascript']);
     gulp.watch("*.html").on('change', bs.reload);
 
-});
+    });
 
